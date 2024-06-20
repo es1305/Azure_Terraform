b091892c3fcd3dfe1e61bbb08cdd459cd8258e7b
es1305 <71124766+es1305@users.noreply.github.com>
2024-06-20 12:38:01 +0700

    Update zid.sh

--- zid.sh
+++ zid.sh
@@ -1,3 +1,3 @@
-curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=hmdeploy.com&status=active" \
+curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$CLOUDFLARE_DOMAIN&status=active" \
   -H "Authorization: Bearer $CLOUDFLARE_BEARER" \
   -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id'
